b48ffb4b3290c55a3c9499161cc3a06e9b421d57
IsmaeelZia <Ismaeel.zia@outlook.com>
2025-01-25 13:50:47 +0300

    app

--- App-page.js
+++
@@ -1,8 +0,0 @@
-// ... inside the login form's submit event listener, after successful login:
-const userData = {
-    username: username, // From your login form
-    email: email,       // From your login form
-    // ... other user data
-};
-localStorage.setItem('userData', JSON.stringify(userData));
-window.location.href = "user.html"; // Redirect to the user page
